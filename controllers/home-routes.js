bd46e1c47d1bb31ef9324b6d268f02a0c8d1604e
PechMV <Pechm607@gmail.com>
2022-12-28 00:11:44 -0600

    edited home-routes.js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -40,4 +40,67 @@ router.get('/', (req, res) => {
     });
 });
 
-router.get
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirexr('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'created_at',
+            'post_content'
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at' ],
+                include: {
+                    model: User,
+                    attributes: ['username', 'github']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username', 'github']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id' });
+            return;
+        }
+
+        const post = dbPostData.get({ plain: true });
+        
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
